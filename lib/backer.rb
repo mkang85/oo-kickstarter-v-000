fd6d76cca811436795017983897780c91b603eed
Michael Kang <michaelkang85@gmail.com>
2018-02-19 17:36:23 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Backer
   attr_accessor :name, :backed_projects
 
@@ -8,5 +10,6 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
+    binding.pry
   end
 end
